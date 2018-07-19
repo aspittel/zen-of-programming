23b25287139064f28ddcf477b22251e0df658d55
Ali Spittel <ali07cat07@gmail.com>
2018-07-19 16:02:31 -0400

    fix favicon

--- src/html.js
+++ src/html.js
@@ -29,8 +29,8 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
           {this.props.headComponents}
           {css}
         </head>
