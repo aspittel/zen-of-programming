316d767ae7d1d78559f065fbbca73766694cd06e
Ali Spittel <ali07cat07@gmail.com>
2018-07-19 16:06:49 -0400

    favicon

--- src/html.js
+++ src/html.js
@@ -29,8 +29,7 @@ module.exports = class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+          <link rel="icon" href="/favicon.ico">
           {this.props.headComponents}
           {css}
         </head>
