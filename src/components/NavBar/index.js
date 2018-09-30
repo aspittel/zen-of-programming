1735707eab59abbe7e3d029debdb4ec9b79e27f8
Ali Spittel <ali07cat07@gmail.com>
2018-09-30 12:06:26 -0400

    add uses page

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,6 +9,7 @@ const NavBar = () => (
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/contact">Contact</Link>
+      <Link to="/uses">Uses</Link>
     </span>
 
     <span className="social-links">
---
+++ src/pages/uses.js
@@ -0,0 +1,78 @@
+import React from "react"
+
+const UsesPage = _ => (
+  <div className="uses">
+    <h1>Uses</h1>
+    <p>I get questions pretty regularly about various pieces of my setup, so I thought I would create a uses page inspired by <a href="https://wesbos.com/uses/">Wes Bos's</a>! I will try and keep this updated.</p>
+
+    <p><img src="/desktop.jpg" alt="my desktop setup with computer, desk, and fairy lights" /></p>
+
+    <p><em>This post contains affiliate links. If you buy something through those links I may earn a small commission at no cost to you. This helps pay for the costs associated with running The Zen of Programming. These are all products I use on a daily basis and love!</em></p>
+
+    <h2 id="texteditor">Text Editor</h2>
+
+    <p><img src="/vs-code/fairy-floss.png" alt="My Visual Studio Code setup with the fairy floss theme and fira code font" /></p>
+
+    <p>I have a <a href="https://zen-of-programming.com/vs-code-setup">full post</a> on my text editor setup; however, I use <a href="https://code.visualstudio.com/">Visual Studio Code</a>. I use <a href="https://github.com/tonsky/FiraCode">Fira Code</a> for my font, and I switch between the <a href="https://marketplace.visualstudio.com/items?itemName=nopjmp.fairyfloss">Fairy Floss</a> and <a href="https://draculatheme.com/visual-studio-code/">Dracula</a> themes. Both are excellent on a projector!</p>
+
+    <h2 id="terminalsetup">Terminal Setup</h2>
+
+    <p><img src="/terminal/terminal.png" alt="my zsh and iterm terminal setup" /></p>
+
+    <p>I also have a <a href="https://zen-of-programming.com/terminal-setup">full post</a> on my terminal setup, but the short version is that I use <a href="https://github.com/robbyrussell/oh-my-zsh">Zsh</a> and <a href="https://www.iterm2.com/">iTerm</a>!</p>
+
+    <h2 id="desktopapps">Desktop Apps</h2>
+
+    <ul>
+      <li>I use <a href="https://www.sketchapp.com/">Sketch</a> for design work and creating my infographics and post headers.</li>
+
+      <li>I use <a href="https://www.ticktick.com/">TickTick</a> as my to do list app, I really like the recurring tasks.</li>
+
+      <li>I use <a href="https://flexibits.com/fantastical">Fantastical</a> for syncing my three Google calendars.</li>
+
+      <li>I am completely reliant on <a href="https://www.spectacleapp.com/">Spectacle</a> for desktop management! I really struggle on computers where I can't use the keyboard shortcuts for moving windows around.</li>
+
+      <li>I use <a href="https://grammarly.go2cloud.org/SH1S5">Grammarly Pro</a> for proofreading my blog posts -- the free version is great as well, but I love how you can customize your tone in the paid version.</li>
+    </ul>
+
+    <h2 id="bloggingtools">Blogging Tools</h2>
+
+    <ul>
+      <li>I use <a href="https://carbon.now.sh/">Carbon</a> for screen shotting code.</li>
+
+      <li>This site is written with <a href="https://www.gatsbyjs.org/">Gatsby.js</a> and uses <a href="https://www.netlify.com/">Netlify</a> for hosting.</li>
+
+      <li>I buy my domain names through <a href="https://namecheap.pxf.io/c/1302279/386494/5618">NameCheap</a></li>
+    </ul>
+
+    <h2 id="computer">Computer</h2>
+
+    <ul>
+      <li>I use the <a href="https://amzn.to/2xgee7g">MacBook Pros with touchbar</a>.</li>
+
+      <li>I have a <a href="https://amzn.to/2MzFxOT">Acer G277HL monitor</a> on my desk at home.</li>
+
+      <li>All of my backgrounds are from <a href="http://www.designlovefest.com/category/downloads/">Design Love Fest</a>. They post new ones each week!</li>
+    </ul>
+
+    <h2 id="office">Office</h2>
+
+    <p>I made my own desk with a few different Ikea pieces!</p>
+
+    <ul>
+      <li><a href="https://www.ikea.com/us/en/catalog/products/90409532/#/10192824">Alex Drawers</a></li>
+
+      <li><a href="https://www.ikea.com/us/en/catalog/products/70353734/">Linnmon Tabletop</a></li>
+
+      <li><a href="https://www.ikea.com/us/en/catalog/products/30345717/">Finnvard Trestle</a></li>
+    </ul>
+
+    <p>My desk chair isn't sold anymore, but its a grey padded chair with wooden legs from Target.</p>
+
+    <p>I have <a href="https://amzn.to/2xcTkWW">fairy lights</a>, succulents, and Flower Pots that store art supplies on my desk!</p>
+
+    <p>The art above my desk is made by <a href="https://riflepaperco.com/">Rifle Paper Co.</a>.</p>
+  </div>
+)
+
+export default UsesPage
